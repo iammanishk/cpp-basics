8e7b70a6cf7be0fa3008ba1dad2f1fc5175d9728
iammanishk <iammaanisshhh@gmail.com>
2023-01-22 01:45:39 +0530

    while loop's basics

---
+++ 0.4while_loop.cpp
@@ -0,0 +1,50 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+    // print numbers from 1 to n;
+   /*int n;
+    cin>>n;
+
+    int i = 1;
+
+    while (i<=n)
+    {
+        cout<<"the numbers are:-"<<i<<endl;
+        i = i+1;
+    }
+    */ 
+
+    //    sum of n numbers
+
+   /* int n;
+    cin>>n;
+    int i = 1;
+    int sum = 0;
+
+    while (i<=n)
+    {
+        sum = sum+i;
+        i = i+1;
+    }
+    cout<<"Sum is :- " << sum << endl;
+    */
+
+//prim or note?
+   /* int n;
+    cin>>n;
+
+    int i = 2;
+     while (i<n)
+     {
+        if(n%i==1){
+            cout<<"Not prime for :- "<< i <<endl;
+        }
+        else{
+            cout<<"Prime for :- "<< i << endl;
+        }
+        i = i + 1;
+     }
+     */
+    return 0;
+}
