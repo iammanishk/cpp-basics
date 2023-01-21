22ec9186b568cc5ccff11ba80b54b9ef79c8d9da
iammanishk <iammaanisshhh@gmail.com>
2023-01-22 02:06:21 +0530

    all the question on pattern problem are here

---
+++ 0.5_Pattern_Problems.cpp
@@ -0,0 +1,42 @@
+#include<iostream>  
+using namespace std;
+
+int main(){
+    //PATTERN -- 01;
+
+   /* int n;
+    cin >> n;
+    int i = 1;
+
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            cout<<"*";
+            j = j+1;
+        }
+        cout<<endl;
+        i = i +1;
+    }
+    */
+    //PATTERN -- 02;
+    int n;
+    cin>>n;
+    int i = 1;
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            cout<<i;
+            j = j + 1;
+        }
+        cout<<endl;
+        i = i+1;
+    }
+    
+
+    
+    return 0;
+}
